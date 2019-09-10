b6bf2bb1e84c2fc44de61f668616befcfdc1aa0f
Jagdish Patil <jagdishpatil316@gmail.com>
2019-09-10 08:16:52 +0000

    modified

--- example.tf
+++ example.tf
@@ -1,9 +1,9 @@
 provider "aws" {
   profile    = "default"
-  region     = "us-west-2"
+  region     = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-033a024887b09d8a8"
-  instance_type = "t1.micro"
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
 }
