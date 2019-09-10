f69226f7eabd9cc02903f824bfe120da6d2c69a4
Jagdish Patil <jagdishpatil316@gmail.com>
2019-09-10 08:21:01 +0000

    modified

--- example.tf
+++ example.tf
@@ -6,4 +6,6 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["vpc-a89946cd"]
+  subnet_id = "subnet-025c622a"
 }
